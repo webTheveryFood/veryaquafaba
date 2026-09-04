5342d6a924b9e4c50d91a260aa2b3808c53015f4
EstebanAleart <esteban.aleart@gmail.com>
2026-09-04 13:50:53 +0200

    feat: add Google Analytics 4 gtag (G-ND2TPEYLRH) to head

--- app/layout.js
+++ app/layout.js
@@ -63,6 +63,9 @@ export default function RootLayout({ children }) {
       <head>
         {/* Google Tag Manager */}
         <script dangerouslySetInnerHTML={{ __html: "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-MT3HRS99');" }} />
+        {/* Google tag (gtag.js) — GA4 */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-ND2TPEYLRH" />
+        <script dangerouslySetInnerHTML={{ __html: "window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-ND2TPEYLRH');" }} />
         <link rel="stylesheet" href="https://use.typekit.net/erx6ovq.css" />
         {frozenStyles.map((href) => <link key={href} rel="stylesheet" href={href} />)}
         <link rel="stylesheet" href="/wp-custom.css" />
